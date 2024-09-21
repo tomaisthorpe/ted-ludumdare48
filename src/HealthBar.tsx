ca2e934997a932f2cc04189427a9ff1d6a748ba1
Tom Aisthorpe <tomaisthorpe@gmail.com>
2024-09-21 17:39:10 +0100

    fix: adjust health bar position

--- src/HealthBar.tsx
+++ src/HealthBar.tsx
@@ -2,7 +2,7 @@ import { useGameContext, useUIContext } from "@tedengine/ted";
 
 const healthColor = "rgb(177, 7, 7)";
 const healthBarWidth = 250;
-const healthBarHeight = 20;
+const healthBarHeight = 19;
 
 export function HealthBar() {
   const { scaling } = useUIContext();
@@ -11,40 +11,47 @@ export function HealthBar() {
   return (
     <div
       style={{
-        display: "flex",
         position: "absolute",
-        top: "16px",
-        right: "16px",
-        alignItems: "center",
+        top: "0",
+        right: "0",
         transform: `scale(${scaling})`,
         transformOrigin: "top right",
-        pointerEvents: "none",
       }}
     >
       <div
         style={{
-          marginRight: "8px",
-          color: healthColor,
-          fontSize: "16px",
-        }}
-      >
-        Health
-      </div>
-      <div
-        style={{
-          width: `${healthBarWidth}px`,
-          height: `${healthBarHeight - 4}px`,
-          border: `2px solid ${healthColor}`,
-          padding: "4px",
+          display: "flex",
+          marginTop: "12px",
+          marginRight: "48px",
+          alignItems: "center",
+          pointerEvents: "none",
         }}
       >
         <div
           style={{
-            width: `${healthBarWidth * (ctx.health / 100)}px`,
-            height: `${healthBarHeight - 4}px`,
-            background: healthColor,
+            marginRight: "8px",
+            color: healthColor,
+            fontSize: "16px",
           }}
-        />
+        >
+          Health
+        </div>
+        <div
+          style={{
+            width: `${healthBarWidth}px`,
+            height: `${healthBarHeight - 4}px`,
+            border: `2px solid ${healthColor}`,
+            padding: "4px",
+          }}
+        >
+          <div
+            style={{
+              width: `${healthBarWidth * (ctx.health / 100)}px`,
+              height: `${healthBarHeight - 4}px`,
+              background: healthColor,
+            }}
+          />
+        </div>
       </div>
     </div>
   );
